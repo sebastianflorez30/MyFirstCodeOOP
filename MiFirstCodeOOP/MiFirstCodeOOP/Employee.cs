1b69993f39f2a93526e1cbbe85bbd0e9fa3d29b1
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-02-20 00:51:00 -0500

    mi primer codigo con una clase nueva Employee

---
+++ MiFirstCodeOOP/MiFirstCodeOOP/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDate { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+
+        #region Methods
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return String.Format("--EMPLOYEE--\n\t" +
+                "Id ttettsdrehshsh: {0} \n\t" +
+                "Last Name: {2} \n\t" +
+                "First Name: {1} \n\t " +
+                "Birth Day: {3} \n\t " +
+                "Hiring Date: {4} \n\t " +
+                "Is Active? {5}",
+                Id,
+                FirstName,
+                LastName,
+                BirthDate,
+                HiringDate,
+                IsActive);
+
+            //return $"------EMPLOYEE------\n\t" +
+            //    $"ID: {Id} \n\t " +
+            //    $"First Name: {FirstName} \n\t " +
+            //    $"Last Name: {LastName} \n\t " +
+            //    $"Birth date: {BirthDate} \n\t " +
+            //    $"Hiring Date: {HiringDate} \n\t " +
+            //    $"Is Active? {IsActive}";
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
