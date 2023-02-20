17c88b4c2c54e4dee428f3b6af5993eee9900c5c
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-02-20 01:02:05 -0500

    crear nueva clase SalaryEmployee

---
+++ MiFirstCodeOOP/MiFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t" +
+                $"Value to Pay: {GetValueToPay()}";
+        }
+
+        #endregion
+    }
+}
