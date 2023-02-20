80051d7ae7f1d18287619299fbdab2eabf9644ff
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-02-20 00:02:26 -0500

    yo cree mi primer codigo con una nueva clase date

---
+++ MiFirstCodeOOP/MiFirstCodeOOP/Date.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    public class Date
+    {
+        #region Properties
+        public int _year;
+        public int _month;
+        public int _day;
+        #endregion
+        
+        #region Constructor
+
+        public Date(int year, int month, int day)
+        {
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);
+            _day = ValidateDay(year, month, day);
+        }
+        #endregion
+
+        #region Methods
+
+        private int ValidateYear(int year)
+        {
+            if (year >= 1900)
+            {
+                return year;
+            }
+
+            throw new YearException(String.Format("The year {0} is invalid.", year));
+        }
+
+        private int ValidateDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException(String.Format("The day {0} doesn't exist for month {1}.", day, month));
+        }
+
+        private int ValidateMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException(String.Format("The month {0} is invalid.", month));
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0:00}/{1:00}/{2:00}", _year, _month, _day);
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+        #endregion
+    }
+}
---
+++ MiFirstCodeOOP/MiFirstCodeOOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Date dateObject = new Date(2023, 2, 29);
+                Console.WriteLine("OOP CONCEPTS");
+                Console.WriteLine("************");
+
+                Console.WriteLine("Please put your birth date year:");
+                var year = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Please put your birth date month:");
+                var month = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Please put your birth date day:");
+                var day = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
+                Console.WriteLine("");
+                Console.WriteLine("******Testing latest implementation*******");
+                Console.WriteLine("");
+
+                Console.WriteLine("Type your ID");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type your first name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Type your last name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Are you active?");
+                bool isActive = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("Enter your salary:");
+                decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                Employee salaryEmployee = new SalaryEmployee()
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)),
+                    HiringDate = new Date(2022, 3, 4),
+                    IsActive = isActive,
+                    Salary = salary,
+                };
+
+                Console.WriteLine(salaryEmployee);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
