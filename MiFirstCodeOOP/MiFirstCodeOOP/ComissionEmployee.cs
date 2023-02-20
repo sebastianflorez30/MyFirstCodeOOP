8138584731a83232e5b9612fb40a79fa68929e7d
SEBASTIAN FLOREZ RAMIREZ <sebastianflorez303254@correo.itm.edu.co>
2023-02-20 18:39:56 -0500

    edicion

---
+++ MiFirstCodeOOP/MiFirstCodeOOP/ComissionEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiFirstCodeOOP
+{
+    internal class ComissionEmployee
+    {
+        #region Properties
+
+        #endregion
+
+    }
+}
